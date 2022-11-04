82ced1116b4e35206409a0c6678435a2d1afe2f6
work <work@moosh-air.local>
2022-11-04 16:48:02 +0900

    Updated

--- zennichi.py
+++ zennichi.py
@@ -19,22 +19,23 @@ path = os.getcwd()
 td_name_list = []
 td_details_list = []
 
+# japan prefecture tuplets 
 prefectures = [('01', '北海道'), ('02', '青森県'), ('03', '岩手県'), ('04', '宮城県'), ('05', '秋田県'), ('06', '山形県'), ('07', '福島県'), ('08', '茨城県'), ('09', '栃木県'), ('10', '群馬県'), ('11', '埼玉県'), ('12', '千葉県'), ('13', '東京都'), ('14', '神奈川県'), ('15', '山梨県'), ('16', '新潟県'), ('17', '富山県'), ('18', '石川県'), ('19', '福井県'), ('20', '長野県'), ('21', '岐阜県'), ('22', '静岡県'), ('23', '愛知県'), ('24', '三重県'), ('25', '滋賀県'), ('26', '京都府'), ('27', '大阪府'), ('28', '兵庫県'), ('29', '奈良県'), ('30', '奈良県'), ('31', '鳥取県'), ('32', '島根県'), ('33', '岡山県'), ('34', '広島県'), ('35', '山口県'), ('36', '徳島県'), ('37', '香川県'), ('38', '愛媛県'), ('39', '高知県'), ('40', '福岡県'), ('41', '佐賀県'), ('42', '長崎県'), ('43', '熊本県'), ('44', '大分県'), ('45', '宮崎県'), ('46', '鹿児島県'), ('47', '沖縄県') ]
 
-for i in prefectures:
 
+# loop all prefectures 
+for i in prefectures:
     # webdriver 
     url = 'https://www.zennichi.or.jp/member_search/list/?prefecture={}&branch=&address=&representative=&shogo=&shogo_kana=&license_holder=&number=&region=&hosho_approved='.format(i)
     driver = webdriver.Chrome()
     driver.get(url)
 
-    # prefecture name
+    # find prefecture name
     prefecture = driver.find_element(By.XPATH, '//*[@id="prefecture"]/option[@selected="selected"]').text
     print('Starting: ' + str(prefecture) + '...')
 
     try:
         while driver.find_element(By.CLASS_NAME, 'next-btn'):
-
             # table
             member_result_table = driver.find_element(By.CLASS_NAME, 'member-result-table')
             # tr 
@@ -54,6 +55,7 @@ for i in prefectures:
             next_button.click()
             time.sleep(1)
 
+    # once last time for the last page, since it doesn't have the next button
     except NoSuchElementException:
         # table
         member_result_table = driver.find_element(By.CLASS_NAME, 'member-result-table')
@@ -69,6 +71,8 @@ for i in prefectures:
             td_details = str(td_details.text)
             td_details_list.append(td_details)
 
+    driver.close()
+    time.sleep(5)
 
     # dictionary of lists  
     dict = {'name': td_name_list, 'details': td_details_list}  
