80eae49f7b5f495e4bc1ff1ef8b3f7b9de4c7204
work <work@moosh-air.local>
2022-11-04 14:27:08 +0900

    Updated

--- zennichi.py
+++ zennichi.py
@@ -20,14 +20,17 @@ td_name_list = []
 td_details_list = []
 
 
+prefecture_range = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', 
+                    '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+                    '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
+                    '31', '32', '33', '34', '35', '36', '37', '38', '39', '40',
+                    '41', '42', '43', '44', '45', '46', '47',]
 
-for i in range(1, 48):
-
-    # prefecture number 01~47
-    prefecture_number = i
+# for i in range(1, 48):
+for i in prefecture_range:
 
     # webdriver 
-    url = 'https://www.zennichi.or.jp/member_search/list/?prefecture={}&branch=&address=&representative=&shogo=&shogo_kana=&license_holder=&number=&region=&hosho_approved='.format(prefecture_number)
+    url = 'https://www.zennichi.or.jp/member_search/list/?prefecture={}&branch=&address=&representative=&shogo=&shogo_kana=&license_holder=&number=&region=&hosho_approved='.format(i)
     driver = webdriver.Chrome()
     driver.get(url)
 
