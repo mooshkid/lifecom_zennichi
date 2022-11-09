fb5b5055eaaddc76bbc9b2184e9e09c092cb7bb3
work <work@moosh-air.local>
2022-11-09 11:24:38 +0900

    Updated

--- test/email_from_csv.py
+++ test/email_from_csv.py
@@ -1,9 +1,14 @@
 import csv
 import re
+import os
+
+# file names
+input_csv = './20221104/prefectures_all.csv'
+output_csv = 'prefectures_all_email.csv'
 
 email_list = []
 
-with open('./20221104/prefectures_all.csv', newline='') as inputfile:
+with open(input_csv, newline='') as inputfile:
     reader = csv.reader(inputfile, delimiter=',')
 
     for row in reader:
@@ -15,6 +20,25 @@ with open('./20221104/prefectures_all.csv', newline='') as inputfile:
             email_list.append(match)
 
 
-with open('prefectures_all_email.csv', 'a') as outputfile:
+# user input yes/no
+yes_choices = ['yes', 'y']
+no_choices = ['no', 'n']
+
+if os.path.exists(output_csv):
+    while True:
+        user_input = input('The file "' + output_csv + '" already exists. Would you like to override it? (yes/no): ')
+        if user_input.lower() in yes_choices:
+            os.remove(output_csv)
+            break
+        elif user_input.lower() in no_choices:
+            new_csv_name = input('Please enter a new file name: ')
+            output_csv = new_csv_name + '.csv'
+            break
+        else:
+            continue
+else:
+    pass
+
+with open(output_csv, 'a') as outputfile:
     writer = csv.writer(outputfile)
     writer.writerows(email_list)
