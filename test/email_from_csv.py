ba66dbf8d12b1008e2a22eeddb83b29f401ee2dd
work <work@moosh-air.local>
2022-11-09 10:50:00 +0900

    Updated

--- test/email_from_csv.py
+++ test/email_from_csv.py
@@ -3,18 +3,18 @@ import re
 
 email_list = []
 
-with open('./20221104/all_details.csv', newline='') as inputfile:
+with open('./20221104/prefectures_all.csv', newline='') as inputfile:
     reader = csv.reader(inputfile, delimiter=',')
 
     for row in reader:
 
         pattern = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b')
-        match = pattern.findall(row[0])
+        match = pattern.findall(row[2]) #2 is the details row
 
         if match:
             email_list.append(match)
 
 
-with open('email.csv', 'a') as outputfile:
+with open('prefectures_all_email.csv', 'a') as outputfile:
     writer = csv.writer(outputfile)
     writer.writerows(email_list)
